cfabbbb80a36e11c2aa9c47ca3b47db4e8f74c3e
Qitmeershehzadghzi <qitmeershehzad@gmail.com>
2026-03-23 07:36:23 +0500

    feat: Add pdf-parse dependency and create the initial user model.

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,9 @@ dotenv.config(); // ✅ sabse pehle
 const app = require("./src/app.js");
 const connectDB = require("./src/config/db.js");
 
-
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 connectDB();
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
